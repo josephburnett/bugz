ef790cb4e3e24bc2d8d42120e69a77a834d51044
Joseph Burnett <josephburnett79@gmail.com>
2017-07-08 22:01:10 +0000

    Build script.

---
+++ bin/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+export ROOT=`git rev-parse --show-toplevel`
+
+cd "${ROOT}"
+bin/genproto.sh
+bin/genclient.sh
+
+cd "${ROOT}/server"
+go build -o "${ROOT}/build/colony" *.go
